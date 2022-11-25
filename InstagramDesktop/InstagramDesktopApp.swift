c81e447079c1ea8410bcd4f5e5d90398d4000d27
alaxhenry <alaxhenry0121@gmail.com>
2022-11-25 11:54:14 +0900

    Initial Commit

---
+++ InstagramDesktop/InstagramDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramDesktopApp.swift
+//  InstagramDesktop
+//
+//  Created by Seungchul Ha on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
